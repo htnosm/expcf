ebb6c988343c168cd32a5e3792f20bd1fd11a778
htnosm <htnosm@gmail.com>
2021-10-17 08:39:58 +0900

    Update README

--- setup.py
+++ setup.py
@@ -3,11 +3,19 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as requirements_file:
     install_requirements = requirements_file.read().splitlines()
 
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
 setup(
     name="expcf",
     version="0.0.1",
     description="Export describe CloudFrontDistribution",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/htnosm/expcf",
     author="htnosm",
+    author_email='htnosm@gmail.com',
+    license='MIT',
     packages=find_packages(),
     install_requires=install_requirements,
     entry_points={
